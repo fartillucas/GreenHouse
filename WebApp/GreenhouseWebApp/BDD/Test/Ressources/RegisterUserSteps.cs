3dc92f16951332eb1849509c12f683f16d6a7729
michaeltrier1 <32192126+michaeltrier1@users.noreply.github.com>
2019-03-21 09:42:12 +0100

    bddtest + registerUser

---
+++ WebApp/GreenhouseWebApp/BDD/Test/Ressources/RegisterUserSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace BDD
+{
+    [Binding]
+    public class RegisterUserSteps
+    {
+        [Given(@"acceptable user credidentials")]
+        public void GivenAcceptableUserCredidentials()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"register is clicked")]
+        public void WhenRegisterIsClicked()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the user is saved in the database")]
+        public void ThenTheUserIsSavedInTheDatabase()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"user is redirected to login screen")]
+        public void ThenUserIsRedirectedToLoginScreen()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
