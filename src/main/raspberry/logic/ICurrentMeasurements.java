2bba7dcf0e72480a4926e5df4c2001411d58317e
Michael poulsen <fartillucas@gmail.com>
2019-03-21 11:53:53 +0100

    CurrentMeasurements interface

---
+++ src/main/raspberry/logic/ICurrentMeasurements.java
@@ -0,0 +1,22 @@
+package raspberry.logic;
+
+public interface ICurrentMeasurements {
+
+	 double getTemp();
+
+	 void setTemp(double temp);
+
+	 double getTemp2();
+
+	 void setTemp2(double temp2);
+
+	 double getMoist();
+
+	 void setMoist(double moist);
+
+	 double getLevel();
+
+	 void setLevel(double level);
+}
+
+
---
+++ src/main/raspberry/logic/currentmeasurements/CurrentMeasurementsFacede.java
@@ -0,0 +1,46 @@
+package raspberry.logic.currentmeasurements;
+
+import raspberry.logic.ICurrentMeasurements;
+
+public class CurrentMeasurementsFacede implements ICurrentMeasurements {
+
+	@Override
+	public double getTemp() {
+		return 0;
+	}
+
+	@Override
+	public void setTemp(double temp) {
+
+	}
+
+	@Override
+	public double getTemp2() {
+		return 0;
+	}
+
+	@Override
+	public void setTemp2(double temp2) {
+
+	}
+
+	@Override
+	public double getMoist() {
+		return 0;
+	}
+
+	@Override
+	public void setMoist(double moist) {
+
+	}
+
+	@Override
+	public double getLevel() {
+		return 0;
+	}
+
+	@Override
+	public void setLevel(double level) {
+
+	}
+}
