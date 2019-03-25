1cd44092ab55fe2fd08693480ecc9133b7b5b440
Michael poulsen <fartillucas@gmail.com>
2019-03-25 10:21:01 +0100

    schedule setpoints

--- src/main/raspberry/logic/schedule/Schedule.java
+++ src/main/raspberry/logic/schedule/Schedule.java
@@ -4,47 +4,84 @@ import raspberry.Acquaintance.ErrorCode;
 import raspberry.Acquaintance.ISchedule;
 import raspberry.logic.SetPoints;
 
-import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.HashMap;
 import java.util.List;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 public class Schedule implements ISchedule {
 
     private static Schedule instance;
+    private double temperature;
 
     private List<HashMap<Integer, SetPoints>> schedule;
-    private Date appliedDate;
+    private LocalDate appliedDate;
+    private LocalDate dateNow;
 
-    public static ISchedule getInstance(){
+
+    public static ISchedule getInstance() {
         //TODO fix singleton vs interface
 
-        if (Schedule.instance == null){
+        if (Schedule.instance == null) {
             Schedule.instance = new Schedule();
         }
 
         return Schedule.instance;
     }
 
-    private Schedule(){
+    private Schedule() {
     }
 
     @Override
     public ErrorCode apply(List<HashMap<Integer, SetPoints>> schedule) {
-        if (schedule != null){
+        if (schedule != null) {
             this.schedule = schedule;
-            this.appliedDate = new Date();
             return ErrorCode.OK;
         } else {
             return ErrorCode.NOTAPPLIED;
         }
     }
 
-    public double getTemperature(Date date){
-
-
-        java.util.Calendar.getInstance();
-
-
-        return temperature;
+    public static LocalDate getMeasurementDate() {
+        return LocalDate.now();
     }
+
+    private LocalTime getTimeOfDay () {
+        return LocalTime.now();
+    }
+
+    private SetPoints getSetpoint(){
+        getMeasurementDate();
+        LocalTime currentTimeOfDay = getTimeOfDay();
+
+        int minutes = currentTimeOfDay.getHour()*60+currentTimeOfDay.getMinute();
+        int block = minutes/120;
+
+
+        long index = DAYS.between(appliedDate, dateNow)%schedule.size();
+
+
+        HashMap<Integer, SetPoints> day = schedule.get((int) index);
+
+        SetPoints setpoints = day.get(block);
+
+        return setpoints;
+    }
+
+    public double getTemperature() {
+        SetPoints setPoints = this.getSetpoint();
+        return setPoints.getTemperature();
+    }
+    public double getHumidity() {
+        SetPoints setPoints = this.getSetpoint();
+        return setPoints.getHumidity();
+    }
+    public double getWaterLevel() {
+        SetPoints setPoints = this.getSetpoint();
+        return setPoints.getWaterlevel();
+    }
+
 }
---
+++ src/test/java/derp/MaintainInternalInvironment.java
@@ -0,0 +1,18 @@
+package derp;
+
+import Mocks.ServerMock;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import raspberry.logic.Starter;
+
+import java.io.IOException;
+
+import static java.lang.Thread.sleep;
+import static junit.framework.TestCase.assertTrue;
+
+public class MaintainInternalInvironment {
+}
+
