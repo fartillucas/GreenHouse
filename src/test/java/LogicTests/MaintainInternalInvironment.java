945dd525d83f719a8477d404ee6f183466759bed
Michael poulsen <fartillucas@gmail.com>
2019-03-27 10:19:57 +0100

    CurrentMeasurements interface

--- src/test/java/LogicTests/MaintainInternalInvironment.java
+++ src/test/java/LogicTests/MaintainInternalInvironment.java
@@ -7,7 +7,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class MaintaininternalInvironment {
+public class MaintainInternalInvironment {
 	@Given("^an initialized system$")
 	public void anInitializedSystem() throws Throwable {
 		// Write code here that turns the phrase above into concrete actions
